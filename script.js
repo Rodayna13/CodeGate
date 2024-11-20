00e20b4c9970436abafd7c1952e508c4732b942b
Rodayna Yasser <104641669+Rodayna13@users.noreply.github.com>
2024-11-20 21:02:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
                     showMore.textContent = " See more";
 
                     // Adjust container height back to 70vh
-                    container.style.height = "70vh";
                 } else {
                     description.classList.add("expanded");
                     description.textContent = fullText;
@@ -27,9 +26,79 @@ document.addEventListener("DOMContentLoaded", () => {
                     showMore.textContent = " See less";
 
                     // Adjust container height to 100vh
-                    container.style.height = "100vh";
                 }
             });
         }
     });
 });
+function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+  window.addEventListener("load", initSwiper);
+
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    new Swiper('.init-swiper', {
+      loop: true,
+      speed: 600,
+      autoplay: {
+        delay: 5000
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 40
+        },
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 60
+        },
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 80
+        },
+        992: {
+          slidesPerView: 5,
+          spaceBetween: 120
+        },
+        1200: {
+          slidesPerView: 6,
+          spaceBetween: 120
+        }
+      }
+    });
+  });
+  
